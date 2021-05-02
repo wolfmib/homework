f096334e0a7b93254424cbfb54d66008d181b9b2
johnny <johnny@johnnys-MacBook-Pro.local>
2021-05-02 15:31:05 +0800

    updated bonjour

--- bonjour.go
+++ bonjour.go
@@ -4,4 +4,10 @@ import "fmt"
 
 func main() {
 	fmt.Println("Back to git!!!")
+	fmt.Println("Back to git!!!")
+	fmt.Println("Back to git!!!")
+	fmt.Println("Back to git!!!")
+	fmt.Println("Back to git!!!")
+	fmt.Println("Back to git!!!")
+	fmt.Println("Back to git!!!")
 }
